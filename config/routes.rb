eabbdfbbd11a2793f5888f2d5195705e9fb94067
tairakaito <get.across.sea8@ezweb.ne.jp>
2020-03-09 10:55:08 +0000

    kadaigo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 get 'users/:id/followed' => 'users#followed', as:'followed'
 get 'users/:id/follower' => 'users#follower', as: 'follower'
+get 'search' => 'search#search', as: 'search'
 devise_for :users
 resources :users,only: [:show,:index,:edit,:update] do
 	resource :relationships, only: [:create, :destroy]
